22fee89d1efe0abb89a7d9dd9f87aacf54202078
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-07-09 17:49:48 -0300

    my first commit

---
+++ src/main/java/com/mrodrigochaves/swiftcash/SwiftcashApplication.java
@@ -0,0 +1,13 @@
+package com.mrodrigochaves.swiftcash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftcashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftcashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrodrigochaves/swiftcash/SwiftcashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrodrigochaves.swiftcash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftcashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
